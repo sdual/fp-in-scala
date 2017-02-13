1eedbae201a5827fb59bafe319e68c9d4db55225
stenoritama <shr.nrht.0623@gmail.com>
2017-02-13 23:12:48 +0900

    implement reverse

--- src/main/scala/chap03/List.scala
+++ src/main/scala/chap03/List.scala
@@ -95,9 +95,29 @@ object List {
     }
   }
 
+  // exercise 3.10
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(x, z))(f)
+    }
+  }
+
   def sum2(ns: List[Int]) = foldRight(ns, 0)((x, y) => x + y)
 
+  // exercise 3.11
+  def sum3(ns: List[Int]) = foldLeft(ns, 0)((x, y) => x + y)
+
   def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
+
+  // exercise 3.11
+  def product3(ns: List[Double]) = foldLeft(ns, 1.0)(_ * _)
+
+  // exercise 3.12
+  def reverse[A](ns: List[A]): List[A] = {
+    foldLeft(ns, Nil: List[A])((x, y) => Cons(x, y))
+  }
 
   def length[A](as: List[A]): Int = {
     foldRight(as, 0)((_, n) => n + 1)
@@ -113,6 +133,6 @@ object List {
 object Test extends App {
   val as = List(2, 4, 6, 8, 10, 3, 5, 8)
 
-  println(List.length(as))
+  println(List.reverse(as))
 
 }
